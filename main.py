d8245561371dd6c71875b494ee45b5e9a1b32461
Justin Hill <jhill@figma.com>
2025-09-20 13:18:58 -0700

    Add gemini configuration

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from python-ai-agent!")
+import os
+from dotenv import load_dotenv
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
 if __name__ == "__main__":
     main()
